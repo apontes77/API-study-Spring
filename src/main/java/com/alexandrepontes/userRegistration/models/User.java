7e385009e16687f0f5d0dbe835bd908123eb321f
apontes77 <alexandrepontesgyn@gmail.com>
2020-12-29 19:54:02 -0300

    feat: creating a model class for User

---
+++ src/main/java/com/alexandrepontes/userRegistration/models/User.java
@@ -0,0 +1,82 @@
+package com.alexandrepontes.userRegistration.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.Constraint;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "TB_USER")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    @NotBlank(message = "nome é obrigatório")
+    private String name;
+
+    @NotBlank(message = "CPF é obrigatório")
+    @Column(unique = true)
+    private String CPF;
+
+    @NotBlank(message = "e-mail é obrigatório")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "data de nascimento é obrigatória")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private Date birthDay;
+
+    public User() {
+    }
+
+    public User(String name, String CPF, String email, Date birthDay) {
+        this.name = name;
+        this.CPF = CPF;
+        this.email = email;
+        this.birthDay = birthDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+
+}
